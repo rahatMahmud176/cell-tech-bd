7a99d6aa901ff11e7b5fc5912d683b915fa93345
rahatMahmud <rahatmahmud176@gmail.com>
2022-09-20 06:40:38 +0600

    20-09-2022

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,8 @@
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|test 
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/* 
+20-9-2022
 */
 
 
