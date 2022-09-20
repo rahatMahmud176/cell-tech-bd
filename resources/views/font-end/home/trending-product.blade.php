6925dd7216e752492b28a040982908fadb1c251c
rahatMahmud <rahatmahmud176@gmail.com>
2022-09-20 23:45:23 +0600

    20-09-22

--- resources/views/font-end/home/trending-product.blade.php
+++ resources/views/font-end/home/trending-product.blade.php
@@ -1,47 +1,51 @@
 <section class="trending-product section">
     <div class="container">
-    <div class="row">
-    <div class="col-12">
-    <div class="section-title">
-    <h2>Trending Product</h2>
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have
-    suffered alteration in some form.</p>
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>Trending Product</h2>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have
+                        suffered alteration in some form.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+
+            @foreach ($trandingProducts as $item)
+                <div class="col-lg-3 col-md-6 col-6">
+                    <div class="single-product">
+                        <div class="product-image">
+                            <a href="{{ route('product-details', ['id' => $item->id]) }}">
+                                <img src="{{ $item->image }}" alt="Image">
+                            </a>
+
+                            <div class="button">
+                                <a href="product-details.html" class="btn"><i class="lni lni-cart"></i> Add to
+                                    Cart</a>
+                            </div>
+                        </div>
+                        <div class="product-info">
+                            <span class="category">{{ $item->category->title }}</span>
+                            <h4 class="title">
+                                <a href="{{ route('product-details', ['id' => $item->id]) }}">{{ $item->title }}</a>
+                            </h4>
+                            <ul class="review">
+                                <li><i class="lni lni-star-filled"></i></li>
+                                <li><i class="lni lni-star-filled"></i></li>
+                                <li><i class="lni lni-star-filled"></i></li>
+                                <li><i class="lni lni-star-filled"></i></li>
+                                <li><i class="lni lni-star-filled"></i></li>
+                                <li><span>5.0 Review(s)</span></li>
+                            </ul>
+                            <div class="price">
+                                <span> ৳:{{ $item->sell_price }}/-</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+
+        </div>
     </div>
-    </div>
-    </div>
-    <div class="row">
-    
-@foreach ($trandingProducts as $item) 
-    <div class="col-lg-3 col-md-6 col-12"> 
-    <div class="single-product">
-    <div class="product-image">
-    <img src="{{ $item->image }}" alt="#">
-    <div class="button">
-    <a href="product-details.html" class="btn"><i class="lni lni-cart"></i> Add to Cart</a>
-    </div>
-    </div>
-     <div class="product-info">
-    <span class="category">{{ $item->category->title }}</span>
-    <h4 class="title">
-    <a href="{{ route('product-details',['id'=>$item->id]) }}">{{ $item->title }}</a>
-    </h4>
-    <ul class="review">
-    <li><i class="lni lni-star-filled"></i></li>
-    <li><i class="lni lni-star-filled"></i></li>
-    <li><i class="lni lni-star-filled"></i></li>
-    <li><i class="lni lni-star-filled"></i></li>
-    <li><i class="lni lni-star-filled"></i></li>
-    <li><span>5.0 Review(s)</span></li>
-    </ul>
-    <div class="price">
-    <span> ৳:{{ $item->sell_price }}/-</span>
-    </div>
-    </div>
-    </div> 
-    </div>
-@endforeach
-     
-     
-    </div>
-    </div>
-    </section>
+</section>
